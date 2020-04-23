f6b72ee5a3828fb6488e44733a353c697a2a3151
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-23 16:24:06 -0700

    initial cat pending_tasks

---
+++ elasticsearch_api/src/cat/pending_tasks.rs
@@ -0,0 +1,28 @@
+use url::Url;
+use reqwest::{Request, Method};
+
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/cat-pending-tasks.html
+
+#[allow(dead_code)]
+fn get(base_url: Url) -> Request {
+    let method = Method::GET;
+    let url = base_url.join("/_cat/pending_tasks").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+
+    #[test]
+    fn get_pending_tasks() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let request = get(base);
+        let request_url = request.url();
+
+        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/_cat/pending_tasks");
+        assert_eq!(request_url.path(), "/_cat/pending_tasks");
+    }
+}
