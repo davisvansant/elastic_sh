9c4a04993050c0afa21d08f90582c38a9660af3b
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-23 16:21:43 -0700

    initial cat fielddata

---
+++ elasticsearch_api/src/cat/fielddata.rs
@@ -0,0 +1,28 @@
+use url::Url;
+use reqwest::{Request, Method};
+
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/cat-fielddata.html
+
+#[allow(dead_code)]
+fn get(base_url: Url) -> Request {
+    let method = Method::GET;
+    let url = base_url.join("/_cat/fielddata").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+
+    #[test]
+    fn get_fielddata() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let request = get(base);
+        let request_url = request.url();
+
+        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/_cat/fielddata");
+        assert_eq!(request_url.path(), "/_cat/fielddata");
+    }
+}
