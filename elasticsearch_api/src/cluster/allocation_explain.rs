f7c14a28b2a6e5309a374c1b5b4f2741e8c73d1c
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-24 09:41:48 -0700

    updated cluster allocation explain to use _cluster(vs _cat) request endpoint

--- elasticsearch_api/src/cluster/allocation_explain.rs
+++ elasticsearch_api/src/cluster/allocation_explain.rs
@@ -6,7 +6,7 @@ use reqwest::{Request, Method};
 #[allow(dead_code)]
 fn get(base_url: Url) -> Request {
     let method = Method::GET;
-    let url = base_url.join("/_cat/allocation/explain").unwrap();
+    let url = base_url.join("/_cluster/allocation/explain").unwrap();
 
     reqwest::Request::new(method, url)
 }
@@ -22,7 +22,7 @@ mod tests {
         let request = get(base);
         let request_url = request.url();
 
-        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/_cat/allocation/explain");
-        assert_eq!(request_url.path(), "/_cat/allocation/explain");
+        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/_cluster/allocation/explain");
+        assert_eq!(request_url.path(), "/_cluster/allocation/explain");
     }
 }
