c1641f9e8bd2fdb751d6fcbfefdf2d12ef90dcaa
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-28 12:13:02 -0700

    initial nodes stats

---
+++ elasticsearch_api/src/nodes/stats.rs
@@ -0,0 +1,39 @@
+use url::Url;
+use reqwest::{Request, Method};
+
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-nodes-stats.html
+
+#[allow(dead_code)]
+fn get(base_url: Url) -> Request {
+    let method = Method::GET;
+    let url = base_url.join("/_nodes/stats").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+    use reqwest::{Client, StatusCode};
+
+    #[test]
+    fn get_stats() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let request = get(base);
+        let request_url = request.url();
+
+        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/_nodes/stats");
+        assert_eq!(request_url.path(), "/_nodes/stats");
+    }
+
+    #[tokio::test]
+    async fn stats_response() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let client = Client::new();
+        let request = get(base);
+        let response = client.execute(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+}
