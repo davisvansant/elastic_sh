6278e9012638683df3e30f0feb6ca30e1019a43e
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-01 13:15:57 -0700

    initial index close api

---
+++ elasticsearch_api/src/index/close.rs
@@ -0,0 +1,39 @@
+use url::Url;
+use reqwest::{Request, Method};
+
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-close.html
+
+#[allow(dead_code)]
+fn post(base_url: Url) -> Request {
+    let method = Method::POST;
+    let url = base_url.join("/index/_close").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+    use reqwest::{Client, StatusCode};
+
+    #[test]
+    fn get_index_close_url() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let request = post(base);
+        let request_url = request.url();
+
+        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/index/_close");
+        assert_eq!(request_url.path(), "/index/_close");
+    }
+
+    #[tokio::test]
+    async fn index_close_post() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let client = Client::new();
+        let request = post(base);
+        let response = client.execute(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+}
