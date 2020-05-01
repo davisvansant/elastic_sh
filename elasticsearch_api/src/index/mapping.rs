0478b3e8d3f648009e7557c6c5a9803d84186f48
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-01 14:12:04 -0700

    initial index mapping api

---
+++ elasticsearch_api/src/index/mapping.rs
@@ -0,0 +1,39 @@
+use url::Url;
+use reqwest::{Request, Method};
+
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-put-mapping.html
+
+#[allow(dead_code)]
+fn put(base_url: Url) -> Request {
+    let method = Method::PUT;
+    let url = base_url.join("/index/_mapping").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+    use reqwest::{Client, StatusCode};
+
+    #[test]
+    fn get_index_mapping_url() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let request = put(base);
+        let request_url = request.url();
+
+        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/index/_mapping");
+        assert_eq!(request_url.path(), "/index/_mapping");
+    }
+
+    #[tokio::test]
+    async fn index_mapping_put() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let client = Client::new();
+        let request = put(base);
+        let response = client.execute(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
+}
