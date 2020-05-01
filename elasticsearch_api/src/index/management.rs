244c1e573c9513b0cddc0b5f7e15426f760041a8
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-01 13:05:44 -0700

    initial index management exists api

--- elasticsearch_api/src/index/management.rs
+++ elasticsearch_api/src/index/management.rs
@@ -4,6 +4,7 @@ use reqwest::{Request, Method};
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-create-index.html
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-delete-index.html
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-get-index.html
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-exists.html
 
 #[allow(dead_code)]
 fn put(base_url: Url) -> Request {
@@ -24,6 +25,14 @@ fn delete(base_url: Url) -> Request {
 #[allow(dead_code)]
 fn get(base_url: Url) -> Request {
     let method = Method::GET;
+    let url = base_url.join("/test_index").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[allow(dead_code)]
+fn head(base_url: Url) -> Request {
+    let method = Method::HEAD;
     let url = base_url.join("/test_index").unwrap();
 
     reqwest::Request::new(method, url)
@@ -70,6 +79,16 @@ mod tests {
         let base = Url::parse("http://elasticsearch:9200").unwrap();
         let client = Client::new();
         let request = get(base);
+        let response = client.execute(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn index_management_head() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let client = Client::new();
+        let request = head(base);
         let response = client.execute(request).await.unwrap();
 
         assert_eq!(response.status(), StatusCode::NOT_FOUND);
