35eef27c52f17f4d0e03590d61753865fe9bd8cd
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-03 16:54:58 -0700

    initial index template put api

---
+++ elasticsearch_api/src/index/template.rs
@@ -0,0 +1,39 @@
+use url::Url;
+use reqwest::{Request, Method};
+
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-templates.html
+
+#[allow(dead_code)]
+fn put(base_url: Url) -> Request {
+    let method = Method::PUT;
+    let url = base_url.join("/_template/index_template").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+    use reqwest::{Client, StatusCode};
+
+    #[test]
+    fn get_index_templete_url() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let request = put(base);
+        let request_url = request.url();
+
+        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/_template/index_template");
+        assert_eq!(request_url.path(), "/_template/index_template");
+    }
+
+    #[tokio::test]
+    async fn index_template_put() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let client = Client::new();
+        let request = put(base);
+        let response = client.execute(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
+}
