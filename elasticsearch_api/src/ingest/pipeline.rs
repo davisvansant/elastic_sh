f311551205c29ca42029c618a4b26f02a8d21f3e
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-05 16:13:54 -0700

    initial ingest pipeline get api

--- elasticsearch_api/src/ingest/pipeline.rs
+++ elasticsearch_api/src/ingest/pipeline.rs
@@ -2,11 +2,20 @@ use url::Url;
 use reqwest::{Request, Method};
 
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/put-pipeline-api.html
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/get-pipeline-api.html
 
 #[allow(dead_code)]
 fn put(base_url: Url) -> Request {
     let method = Method::PUT;
     let url = base_url.join("/_ingest/pipeline/pipeline").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[allow(dead_code)]
+fn get(base_url: Url) -> Request {
+    let method = Method::GET;
+    let url = base_url.join("/_ingest/pipeline").unwrap();
 
     reqwest::Request::new(method, url)
 }
@@ -35,5 +44,15 @@ mod tests {
         let response = client.execute(request).await.unwrap();
 
         assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
+
+    #[tokio::test]
+    async fn ingest_pipeline_get() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let client = Client::new();
+        let request = get(base);
+        let response = client.execute(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
     }
 }
