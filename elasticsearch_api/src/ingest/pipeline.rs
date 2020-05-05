24df97d1a169023a8e2535e4a2ec5fbb24bada03
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-05 16:21:01 -0700

    initial ingest pipeline delete api

--- elasticsearch_api/src/ingest/pipeline.rs
+++ elasticsearch_api/src/ingest/pipeline.rs
@@ -3,6 +3,7 @@ use reqwest::{Request, Method};
 
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/put-pipeline-api.html
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/get-pipeline-api.html
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/delete-pipeline-api.html
 
 #[allow(dead_code)]
 fn put(base_url: Url) -> Request {
@@ -16,6 +17,14 @@ fn put(base_url: Url) -> Request {
 fn get(base_url: Url) -> Request {
     let method = Method::GET;
     let url = base_url.join("/_ingest/pipeline").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[allow(dead_code)]
+fn delete(base_url: Url) -> Request {
+    let method = Method::DELETE;
+    let url = base_url.join("/_ingest/pipeline/pipeline").unwrap();
 
     reqwest::Request::new(method, url)
 }
@@ -54,5 +63,15 @@ mod tests {
         let response = client.execute(request).await.unwrap();
 
         assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn ingest_pipeline_delete() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let client = Client::new();
+        let request = delete(base);
+        let response = client.execute(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
     }
 }
