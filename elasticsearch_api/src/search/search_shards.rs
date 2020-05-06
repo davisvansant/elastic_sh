ec728ce6842beb0bb6b9215370e7797ba5c8991c
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-06 16:37:10 -0700

    initial search shards api

---
+++ elasticsearch_api/src/search/search_shards.rs
@@ -0,0 +1,39 @@
+use url::Url;
+use reqwest::{Request, Method};
+
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/search-shards.html
+
+#[allow(dead_code)]
+fn get(base_url: Url) -> Request {
+    let method = Method::GET;
+    let url = base_url.join("/index/_search_shards").unwrap();
+
+    reqwest::Request::new(method, url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+    use reqwest::{Client, StatusCode};
+
+    #[test]
+    fn get_search_shards_url() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let request = get(base);
+        let request_url = request.url();
+
+        assert_eq!(request_url.as_str(), "http://elasticsearch:9200/index/_search_shards");
+        assert_eq!(request_url.path(), "/index/_search_shards");
+    }
+
+    #[tokio::test]
+    async fn search_shards_get() {
+        let base = Url::parse("http://elasticsearch:9200").unwrap();
+        let client = Client::new();
+        let request = get(base);
+        let response = client.execute(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+}
