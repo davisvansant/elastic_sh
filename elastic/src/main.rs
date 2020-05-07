4c75e49d5d3af9c3df649b31ac2fcb37088fba18
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-07 14:32:08 -0700

    initial elasticsearch cli layout

--- elastic/src/main.rs
+++ elastic/src/main.rs
@@ -1,8 +1,28 @@
-use clap::App;
+use clap::{App, SubCommand};
 
 fn main() {
     App::new("elastic")
         .version("0.1.0")
-        .about("Elastic")
+        .about("| >_ a shell and cli for elastic.co products")
+        .subcommand(SubCommand::with_name("es").about("Elasticsearch api")
+            .subcommands( vec![
+                SubCommand::with_name("cat").about("Elasticsearch cat api")
+                    .display_order(1),
+                SubCommand::with_name("cluster").about("Elasticsearch cluster api")
+                    .display_order(2),
+                SubCommand::with_name("index").about("Elasticsearch index api")
+                    .display_order(3),
+                SubCommand::with_name("ingest").about("Elasticsearch ingest api")
+                    .display_order(4),
+                SubCommand::with_name("multi-document").about("Elasticsearch multi document api")
+                    .display_order(5),
+                SubCommand::with_name("nodes").about("Elasticsearch nodes api")
+                    .display_order(6),
+                SubCommand::with_name("search").about("Elasticsearch search api")
+                    .display_order(7),
+                SubCommand::with_name("single-document").about("Elasticsearch single document api")
+                    .display_order(8),
+            ])
+        )
         .get_matches();
 }
