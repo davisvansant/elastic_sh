29cd8d466a318ac79c39e30422be6d26154f7421
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-05-08 13:57:34 -0700

    initial cat api cli layout

--- elastic/src/main.rs
+++ elastic/src/main.rs
@@ -7,7 +7,28 @@ fn main() {
         .subcommand(SubCommand::with_name("es").about("Elasticsearch api")
             .subcommands( vec![
                 SubCommand::with_name("cat").about("Elasticsearch cat api")
-                    .display_order(1),
+                    .display_order(1)
+                    .subcommands( vec![
+                        SubCommand::with_name("aliases").about("Returns information about currently configured aliases to indices"),
+                        SubCommand::with_name("allocation").about("Provides a snapshot of the number of shards allocated to each data node and their disk space"),
+                        SubCommand::with_name("count").about("Provides quick access to a document count of individual indices or all indices in a cluster"),
+                        SubCommand::with_name("fielddata").about("Returns the amount of heap memory currently used by fielddata on every data node in the cluster"),
+                        SubCommand::with_name("health").about("Returns the health status of a cluster, similar to the cluster health API"),
+                        SubCommand::with_name("indicies").about("Returns high-level information about indices in a cluster"),
+                        SubCommand::with_name("master").about("Returns information about the master node, including the ID, bound IP address, and name"),
+                        SubCommand::with_name("nodeattrs").about("Returns information about custom node attributes"),
+                        SubCommand::with_name("nodes").about("Returns information about a cluster’s nodes"),
+                        SubCommand::with_name("pending-tasks").about("Returns cluster-level changes that have not yet been executed"),
+                        SubCommand::with_name("plugins").about("Returns a list of plugins running on each node of a cluster"),
+                        SubCommand::with_name("recovery").about("Returns information about ongoing and completed shard recoveries"),
+                        SubCommand::with_name("repositories").about("Returns the snapshot repositories for a cluster"),
+                        SubCommand::with_name("segments").about("Returns low-level information about the Lucene segments in index shards"),
+                        SubCommand::with_name("shards").about("The shards command is the detailed view of what nodes contain which shards"),
+                        SubCommand::with_name("snapshots").about("Returns information about the snapshots stored in one or more repositories"),
+                        SubCommand::with_name("task-management").about("Returns information about tasks currently executing in the cluster"),
+                        SubCommand::with_name("templates").about("Returns information about index templates in a cluster"),
+                        SubCommand::with_name("thread_pool").about("Returns thread pool statistics for each node in a cluster")
+                    ]),
                 SubCommand::with_name("cluster").about("Elasticsearch cluster api")
                     .display_order(2),
                 SubCommand::with_name("index").about("Elasticsearch index api")
